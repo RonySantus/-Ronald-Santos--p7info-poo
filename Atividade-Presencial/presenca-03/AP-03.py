191720ba8bb6133ccc4ae055ecc878d640420495
Ronald Santos <ronysantus9@gmail.com>
2021-10-13 23:27:37 -0300

    Atualizado 13/10/2021

---
+++ Atividade-Presencial/presenca-03/AP-03.py
@@ -0,0 +1,32 @@
+classe  Pilha ():
+    
+    def  __init__ ( self ):
+       eu . pilha  = []
+       
+    def  adicionar ( próprio , elemento ):
+       eu . pilha . anexar ( elemento )
+    
+    def  retirar ( self ):
+        se  len ( auto . pilha ) >  0 :
+            retornar a  si mesmo . pilha . pop ( - 1 )
+    
+    def  vazio ( próprio ):
+        retornar a  si mesmo . pilha  == []
+    
+    def  mostrarPilha ( self ):
+        imprimir ( auto . pilha )
+       
+    
+pilha  =  Pilha ()
+
+para  p  no  intervalo ( 5 ):
+    empilhar . adicionar ( p )
+
+empilhar . mostrarPilha ()
+empilhar . retirar ()
+empilhar . mostrarPilha ()
+
+para  p  no  intervalo ( 10 ):
+    empilhar . retirar ()
+
+empilhar . mostrarPilha ()
